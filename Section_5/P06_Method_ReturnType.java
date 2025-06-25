f9eb41f38e6f0c0732f186997761ef08527ffc6a
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-25 09:22:04 +0530

    P6

---
+++ Section_5/P06_Method_ReturnType.java
@@ -0,0 +1,29 @@
+package Section_5;
+
+public class P06_Method_ReturnType {
+
+    // Defining a method for calculate the scores
+     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+          int finalScore = score; // Calculating the score
+          if (gameOver) {
+               finalScore += (levelCompleted * bonus);
+               return finalScore; // return the calculated score
+          }
+          return -1; // if the game is not over then default return value 
+     }
+     public static void main(String[] args) {
+          
+          // Defining the variables
+          boolean gameOver = true;
+          int score = 800;
+          int levelCompleted = 5;
+          int bonus = 100;
+          
+          // Calling the method we just created
+          System.out.println("This is the first method of calculating scores");
+
+          // Printing the return of the calculated score
+          System.out.println("The score is " + calculateScore(gameOver, score, levelCompleted, bonus));
+     }
+}
