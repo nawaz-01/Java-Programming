d16666fc19f270c61acff5033f85eb601f410975
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-25 09:38:26 +0530

    P17

---
+++ Section_6/P17_Coding_Challenge19.java
@@ -0,0 +1,21 @@
+package Section_6;
+
+public class P17_Coding_Challenge19 {
+        public static boolean hasSharedDigit(int num1, int num2) {
+          if (num1 < 10 || num1 > 99 || num2 < 10 || num2 > 99) {
+               return false;
+          }
+          int num1FirstDigit = num1 / 10;
+          int num1LastDigit = num1 % 10;
+          int num2FirstDigit = num2 / 10;
+          int num2LastDigit = num2 % 10;
+          return (num1FirstDigit == num2FirstDigit || num1FirstDigit == num2LastDigit || num1LastDigit == num2FirstDigit || num1LastDigit == num2LastDigit);
+     }
+     public static void main(String[] args) {
+          System.out.println(hasSharedDigit(12, 23));
+          System.out.println(hasSharedDigit(15, 55)); 
+          System.out.println(hasSharedDigit(19, 91)); 
+          System.out.println(hasSharedDigit(12, 45)); 
+          System.out.println(hasSharedDigit(9, 99));  
+     }
+}
