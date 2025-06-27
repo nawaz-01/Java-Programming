e617a346a3181fe073b218b7c08a893b1f298250
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-27 13:46:12 +0530

    Updated

--- Section_7_OOPS/InheritanceChallenge_P2/Main.java
+++ Section_7_OOPS/InheritanceChallenge_P2/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Employee tim = new Employee("tim", "11/11/1985", "01/01/2020");
+        Employee tim = new Employee("Tim", "11/11/1985", 0, "01/01/2020");
         System.out.println(tim);
         System.out.println("Age = " + tim.getAge());
         System.out.println("Pay = " + tim.collectPay());
 
-        SalariedEmployee joe = new SalariedEmployee("joe", "11/11/1990", "03/03/2020", 55000);
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/1990", "03/03/2020", 55000);
         System.out.println(joe);
         System.out.println("Joe's Paycheck = $" + joe.collectPay());
 
