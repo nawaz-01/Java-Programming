261e96fa0966eeb6ae94bf3cbe51db0986faea30
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-07-05 00:01:51 +0530

    Composition

--- Section_8/Main.java
+++ Section_8/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
+        ComputerCase theCase = new ComputerCase("2208", "Dell", "240V");
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, "2540 x 1440");
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 2, "v2.44");
+        System.out.println(theCase);
+        System.out.println(theMonitor);
+        System.out.println(theMotherboard);
+
+        /*
+         * PersonalComputer thePC = new PersonalComputer("2208", "Dell", theCase,
+         * theMonitor, theMotherboard);
+         */
+
+        // System.out.println(thePC.getComputerCase());
+        // System.out.println(thePC.getMonitor());
+
+        // thePC.getMotherboard().loadProgram("Windows 1.0");
+        theMonitor.drawPixelAt(1500, 1200, "red");
+        theMonitor.drawPixelAt(1500, 1200, "blue");
+        theMonitor.drawPixelAt(1500, 1200, "green");
+
+        // thePC.getComputerCase().pressPowerButton();
+        // thePC.getMotherboard().loadProgram("Windows 1.0");
+
+        // thePC.powerUp();
     }
 }
